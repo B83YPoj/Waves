c4b5d15e56d6bf74de169ea8e837994e903d8ffc
B83YPoj <B83YPoj@protonmail.com>
2016-10-20 11:31:58 +0300

    f

--- src/main/scala/com/wavesplatform/Application.scala
+++ src/main/scala/com/wavesplatform/Application.scala
@@ -1,19 +1,24 @@
 package com.wavesplatform
 
 import akka.actor.{ActorSystem, Props}
+import com.wavesplatform.actor.RootActorSystem
 import com.wavesplatform.consensus.WavesConsensusModule
 import com.wavesplatform.http.NodeApiRoute
+import com.wavesplatform.settings._
 import scorex.account.AddressScheme
 import scorex.api.http._
 import scorex.app.ApplicationVersion
 import scorex.consensus.nxt.api.http.NxtConsensusApiRoute
+import scorex.crypto.encode.Base58
 import scorex.network.{TransactionalMessagesRepo, UnconfirmedPoolSynchronizer}
+import scorex.transaction.assets.{IssueTransaction, TransferTransaction}
+import scorex.transaction.state.wallet.{IssueRequest, TransferRequest}
 import scorex.utils.ScorexLogging
 import scorex.waves.http.{DebugApiRoute, WavesApiRoute}
-import com.wavesplatform.settings._
 import scorex.waves.transaction.WavesTransactionModule
+
 import scala.reflect.runtime.universe._
-import com.wavesplatform.actor.RootActorSystem
+import scala.util.Random
 
 class Application(as: ActorSystem, appSettings: WavesSettings) extends {
   override implicit val settings = appSettings
@@ -92,15 +97,55 @@ object Application extends ScorexLogging {
 
       if (application.wallet.privateKeyAccounts().isEmpty)
         application.wallet.generateNewAccounts(1)
+
+      testScript()
+
+      def testScript(): Unit = scala.util.Try {
+        val wallet = application.wallet
+        val sender = wallet.privateKeyAccounts().head
+
+        (1L to Long.MaxValue) foreach { i =>
+          val issue = genIssue()
+          println(issue)
+
+          (1 to 10) foreach { j =>
+            (1 to 102) foreach { k =>
+              val assetId = if (Random.nextBoolean()) Some(issue.assetId) else None
+              val feeAsset = if (Random.nextBoolean()) Some(issue.assetId) else None
+              println(genTransfer(assetId, feeAsset))
+            }
+            Thread.sleep(1000)
+          }
+
+
+        }
+
+        def recipient = application.consensusModule.generators(application.blockStorage.history.lastBlock).head
+
+        def genIssue(): IssueTransaction = {
+          val issue = IssueRequest(sender.address, "testss", "testassets", Random.nextLong(), 2, true, 100000000)
+          application.transactionModule.issueAsset(issue, wallet).get
+        }
+
+        def genTransfer(assetId: Option[Array[Byte]], feeAsset: Option[Array[Byte]]): TransferTransaction = {
+          val r: TransferRequest = TransferRequest(assetId.map(Base58.encode), feeAsset.map(Base58.encode),
+            Random.nextInt(), Random.nextInt(10), sender.address, "", recipient.address)
+
+          application.transactionModule.transferAsset(r, wallet).get
+        }
+
+
+      }
+
     }
+
 
   /**
     * Configure logback logging level according to settings
     */
   private def configureLogging(settings: WavesSettings) = {
-    import ch.qos.logback.classic.LoggerContext
+    import ch.qos.logback.classic.{Level, LoggerContext}
     import org.slf4j._
-    import ch.qos.logback.classic.Level
 
     val lc = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
     val rootLogger = lc.getLogger(Logger.ROOT_LOGGER_NAME)
@@ -111,7 +156,7 @@ object Application extends ScorexLogging {
       case "warn" => rootLogger.setLevel(Level.WARN)
       case "trace" => rootLogger.setLevel(Level.TRACE)
       case _ =>
-        log.warn(s"Unknown loggingLevel = ${settings.loggingLevel }. Going to set INFO level")
+        log.warn(s"Unknown loggingLevel = ${settings.loggingLevel}. Going to set INFO level")
         rootLogger.setLevel(Level.INFO)
     }
   }
